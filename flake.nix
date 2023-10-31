500b62f5483d69322dd37aaefd53b4bd38fe6020
Yureka <yuka@yuka.dev>
2023-10-31 03:22:14 +0100

    flake: don't use simpleFlake

--- flake.nix
+++ flake.nix
@@ -3,28 +3,34 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.simpleFlake {
-      inherit self nixpkgs;
-      name = "nix-yang-tools";
-      overlay = final: prev: {
-        nix-yang-tools = rec {
-          nix-yang-tools = final.callPackage (
-            { rustPlatform, libyang }:
+  outputs = { self, nixpkgs, flake-utils }: {
+    overlays.default = final: prev: rec {
+      nix-yang-tools = final.callPackage (
+        { rustPlatform, libyang }:
 
-            rustPlatform.buildRustPackage {
-              pname = "nix-yang-tools";
-              version = self.shortRev or "dirty-${toString self.lastModifiedDate}";
+        rustPlatform.buildRustPackage {
+          pname = "nix-yang-tools";
+          version = self.shortRev or "dirty-${toString self.lastModifiedDate}";
 
-              src = self;
+          src = self;
 
-              NIX_LDFLAGS = "-L ${libyang}/lib";
+          NIX_LDFLAGS = "-L ${libyang}/lib";
 
-              cargoLock.lockFile = ./Cargo.lock;
-            }
-          ) {};
-          defaultPackage = nix-yang-tools;
-        };
-      };
+          cargoLock.lockFile = ./Cargo.lock;
+        }
+      ) {};
+      default = nix-yang-tools;
     };
+  } //
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = builtins.attrValues self.overlays;
+      };
+    in {
+      packages = {
+        inherit (pkgs) nix-yang-tools default;
+      };
+    });
 }
